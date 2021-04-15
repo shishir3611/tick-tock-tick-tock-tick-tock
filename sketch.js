5a086076aba1cc595bcbae671e326e8cfab97f6a
Sheema Parwaz <Sheemaparwaz@gmail.com>
2021-04-15 15:58:14 +0530

    debugged

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
-var h = hour();
-var m = minute();
-var s = second();
+var h, m, s;
 
 function setup() {
-  createCanvas(800,400);
+  createCanvas(800, 400);
+  h = hour();
+  m = minute();
+  s = second();
 }
 
 function draw() {
-  background(255,255,255);  
+  background(255, 255, 255);
   drawSprites();
-  angleMode(DEGREES)
-  angle = map(0,60,0,360)
+  angleMode(DEGREES);
+  angle = map(s,0, 60, 0, 360);
 }
